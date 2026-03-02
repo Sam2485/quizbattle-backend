7eab2cd0ed74b7fa1b0d4aa337d6582c827434d0
Sam2485 <samarthgangji@gmail.com>
2026-03-02 14:29:19 +0530

    Initial Spring Boot backend structure

---
+++ src/main/java/com/quizbattle/QuizbattleApplication.java
@@ -0,0 +1,14 @@
+package com.quizbattle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizbattleApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(QuizbattleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/quizbattle/QuizbattleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quizbattle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizbattleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
